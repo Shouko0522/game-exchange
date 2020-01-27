130d1ffdd37043a2b58550af056a4fca75366f7d
NiiAyi Quaye <8winds@gmail.com>
2020-01-27 17:07:36 +0900

    created games controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
+  has_many :reservations
   validates :name, presence: true
   validates :price, presence: true
   validates :platform, presence: true
