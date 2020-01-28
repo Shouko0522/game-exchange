ccacd9b40ad8463411974430df1188d7adc3e37b
shouko oka <iamshk0522@gmail.com>
2020-01-28 20:26:35 +0900

    create reservation list

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'games#index'
   get "dashboard", to: "games#dashboard"
+  get "reservation_dashboard", to: "reservations#reservation_dashboard"
   resources :games
   resources :reservations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
