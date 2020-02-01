259baa5f997ef66a29ed0769555be513d42d6e36
Gavin Garza <gaving@live.com>
2020-02-01 12:12:41 +0900

    Finished applying the user avatar feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   # Pundit: white-list approach.
@@ -18,15 +19,14 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-end
 
 #   before_action :authenticate_user!
 
-#   def configure_permitted_parameters
+  def configure_permitted_parameters
 #     # For additional fields in app/views/devise/registrations/new.html.erb
-#     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
 
 #     # For additional in app/views/devise/registrations/edit.html.erb
-#     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-#   end
-# end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+  end
+end
