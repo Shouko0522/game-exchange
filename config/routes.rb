9783a853467e6d9bb640a84c7db86c6504b0112d
Gavin Garza <gaving@live.com>
2020-02-06 14:27:23 +0900

    Created a delete method for reservations and added reservations to game show page

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
 
   get "dashboard", to: "games#dashboard"
   get "reservation_dashboard", to: "reservations#reservation_dashboard"
-
+    
   resources :games do
     resources :reservations, only: [ :new, :create ]
   end
-  resources :reservations, only: [ :index, :show, :reservation_dashboard, :edit, :update ]
+  resources :reservations, only: [ :index, :show, :reservation_dashboard, :edit, :update, :destroy ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
